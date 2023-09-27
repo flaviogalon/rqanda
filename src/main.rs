8a8444232d55d24dbcd239a7317e190a3eb5cfea
Flavio H. Galon <flaviogalon@gmail.com>
2023-09-27 20:26:33 +0100

    Implement trait to initialize QuestionId

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+#[derive(Debug)]
 struct Question {
     id: QuestionId,
     title: String,
     content: String,
     tags: Option<Vec<String>>,
 }
-
+#[derive(Debug)]
 struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
 
 impl Question {
     fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
@@ -18,28 +33,14 @@ impl Question {
     }
 }
 
-impl std::fmt::Display for Question {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        write!(
-            f,
-            "{}, title: {}, content: {}, tags: {:?}",
-            self.id, self.title, self.content, self.tags
-        )
-    }
-}
-
-impl std::fmt::Display for QuestionId {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        write!(f, "id: {}", self.0)
-    }
-}
-
 fn main() {
+    let id = QuestionId::from_str("1").unwrap();
+
     let question = Question::new(
-        QuestionId("1".to_string()),
+        id,
         "First question!".to_string(),
         "Content of the question".to_string(),
         Some(vec!["faq".to_string()]),
     );
-    println!("{}", question);
+    println!("{:?}", question);
 }
