c24a89fd821af76a4d1bd0c87d5bf816ed21cf8d
Flavio H. Galon <flaviogalon@gmail.com>
2023-10-10 20:04:07 +0100

    Add validation logic to questions query string arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use warp::filters::cors::CorsForbidden;
+use warp::reject::Reject;
 use warp::{http::Method, http::StatusCode, Filter, Rejection, Reply};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -38,6 +39,11 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
             error.to_string(),
             StatusCode::FORBIDDEN,
         ))
+    } else if let Some(error) = r.find::<Error>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::RANGE_NOT_SATISFIABLE,
+        ))
     } else {
         Ok(warp::reply::with_status(
             "Route not found".to_string(),
@@ -46,10 +52,76 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     }
 }
 
-async fn get_questions(store: Store) -> Result<impl warp::Reply, warp::Rejection> {
-    let res: Vec<Question> = store.questions.values().cloned().collect();
+#[derive(Debug)]
+enum Error {
+    ParseError(std::num::ParseIntError),
+    MissingParameters,
+    InvertedOrder,
+}
 
-    Ok(warp::reply::json(&res))
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Error::ParseError(ref err) => {
+                write!(f, "Can't parse parameter: {}", err)
+            }
+            Error::MissingParameters => write!(f, "Missing parameter"),
+            Error::InvertedOrder => write!(f, "'start' can't be greater than 'end'"),
+        }
+    }
+}
+
+impl Reject for Error {}
+
+#[derive(Debug)]
+struct Pagination {
+    start: usize,
+    end: usize,
+}
+
+fn extract_pagination(params: HashMap<String, String>, limit: usize) -> Result<Pagination, Error> {
+    if !params.contains_key("start") || !params.contains_key("end") {
+        return Err(Error::MissingParameters);
+    }
+
+    let start: usize = params
+        .get("start")
+        .unwrap()
+        .parse::<usize>()
+        .map_err(Error::ParseError)?;
+    let mut end: usize = params
+        .get("end")
+        .unwrap()
+        .parse::<usize>()
+        .map_err(Error::ParseError)?;
+
+    if start > end {
+        return Err(Error::InvertedOrder);
+    } else if end > limit {
+        end = limit;
+    }
+
+    return Ok(Pagination {
+        start: start,
+        end: end,
+    });
+}
+
+async fn get_questions(
+    query_params: HashMap<String, String>,
+    store: Store,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let limit: usize = store.questions.len();
+
+    if !query_params.is_empty() {
+        let pagination = extract_pagination(query_params, limit)?;
+        let res: Vec<Question> = store.questions.values().cloned().collect();
+        let res = &res[pagination.start..pagination.end];
+        Ok(warp::reply::json(&res))
+    } else {
+        let res: Vec<Question> = store.questions.values().cloned().collect();
+        Ok(warp::reply::json(&res))
+    }
 }
 
 #[tokio::main]
@@ -67,6 +139,7 @@ async fn main() {
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
+        .and(warp::query())
         .and(store_filter)
         .and_then(get_questions)
         .recover(return_error);
