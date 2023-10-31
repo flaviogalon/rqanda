dc4c71356f6188aead0360fb8945aaf52a9298ff
Flavio H. Galon <flaviogalon@gmail.com>
2023-10-31 20:08:12 +0000

    Playing around with documentation

--- src/types/pagination.rs
+++ src/types/pagination.rs
@@ -2,12 +2,32 @@ use std::collections::HashMap;
 
 use crate::error;
 
+/// Pagination struct that is getting extracted from query parameters
 #[derive(Debug)]
 pub struct Pagination {
+    /// Index of the first item that has to be returned
     pub start: usize,
+    /// Index of the last item that has to be returned
     pub end: usize,
 }
 
+/** Extract query parameters from the `/questions` route
+    ## Example query
+    GET requests to this route can have a pagination attached so we just return
+    the questions we need
+
+    `/questions?start=1&end=10`
+
+    ## Example usage
+    ```rust
+    let mut query = HashMap::new();
+    query.insert("start".to_string(), "1".to_string());
+    query.inser("end".to_string(), "10".to_string());
+    let p = types::pagination::extract_pagination(query).unwrap();
+    assert_eq!(p.start, 1);
+    assert_eq!(p.end, 10);
+    ```
+*/
 pub fn extract_pagination(
     params: HashMap<String, String>,
     limit: usize,
@@ -33,8 +53,5 @@ pub fn extract_pagination(
         end = limit;
     }
 
-    return Ok(Pagination {
-        start: start,
-        end: end,
-    });
+    Ok(Pagination { start, end })
 }
